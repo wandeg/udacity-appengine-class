5c07244d65274071d836de0b3ba7214ec8b7eb29
wandeg <jmsmaringa@gmail.com>
2016-03-10 21:16:42 +0300

    Endpoints testing

--- helloworld_api.py
+++ helloworld_api.py
@@ -1,46 +1,49 @@
 """Hello World API implemented using Google Cloud Endpoints.
 
-Contains declarations of endpoint, endpoint methods,
-as well as the ProtoRPC message class and container required
-for endpoint method definition.
+Defined here are the ProtoRPC messages needed to define Schemas for methods
+as well as those methods defined in an API.
 """
+
 import endpoints
 from protorpc import messages
 from protorpc import message_types
 from protorpc import remote
 
-
-# If the request contains path or querystring arguments,
-# you cannot use a simple Message class.
-# Instead, you must use a ResourceContainer class
-REQUEST_CONTAINER = endpoints.ResourceContainer(
-    message_types.VoidMessage,
-    name=messages.StringField(1),
-)
-
-
 package = 'Hello'
 
+class Greeting(messages.Message):
+  """Greeting that stores a message"""
+  message = messages.StringField(1)
 
-class Hello(messages.Message):
-    """String that stores a message."""
-    greeting = messages.StringField(1)
+class GreetingCollection(messages.Message):
+  """Collection of Greetings."""
+  items = messages.MessageField(Greeting, 1, repeated=True)
 
+STORED_GREETINGS = GreetingCollection(items=[
+  Greeting(message='hello world!'),
+  Greeting(message='goodbye world!'),
+  ])
 
-@endpoints.api(name='helloworldendpoints', version='v1')
+@endpoints.api(name='helloworld', version='v1')
 class HelloWorldApi(remote.Service):
-    """Helloworld API v1."""
+  """Helloworld API v1."""
 
-    @endpoints.method(message_types.VoidMessage, Hello,
-      path = "sayHello", http_method='GET', name = "sayHello")
-    def say_hello(self, request):
-      return Hello(greeting="Hello World")
+  @endpoints.method(message_types.VoidMessage, GreetingCollection,
+    path='hellogreeting', http_method='GET',
+    name='greetings.listGreeting')
+  def greetings_list(self, unused_request):
+    return STORED_GREETINGS
 
-    @endpoints.method(REQUEST_CONTAINER, Hello,
-      path = "sayHelloByName", http_method='GET', name = "sayHelloByName")
-    def say_hello_by_name(self, request):
-      greet = "Hello {}".format(request.name)
-      return Hello(greeting=greet)
+  ID_RESOURCE = endpoints.ResourceContainer(
+    message_types.VoidMessage,
+    id=messages.IntegerField(1, variant=messages.Variant.INT32))
 
+  @endpoints.method(ID_RESOURCE, Greeting, path='hellogreeting/{id}', http_method='GET',
+    name='greetings.getGreeting')
+  def greeting_get(self, request):
+    try:
+      return STORED_GREETINGS.items[request.id]
+    except (IndexError, TypeError):
+      raise endpoints.NotFoundException('Greeting %s not found. ' % (request.id,))
 
 APPLICATION = endpoints.api_server([HelloWorldApi])
