7b418c3ca01c82ae22128f4211cdbcae513aab3c
wandeg <jmsmaringa@gmail.com>
2016-03-13 14:19:00 +0300

    Addition of greet by period

--- helloworld_api.py
+++ helloworld_api.py
@@ -15,8 +15,16 @@ from protorpc import remote
 # Instead, you must use a ResourceContainer class
 REQUEST_CONTAINER = endpoints.ResourceContainer(
     message_types.VoidMessage,
-    name=messages.StringField(1),
+    name=messages.StringField(1)
 )
+
+REQUEST_GREETING_CONTAINER = endpoints.ResourceContainer(
+    message_types.VoidMessage,
+    name=messages.StringField(1),
+    period=messages.StringField(2)
+)
+
+
 
 
 package = 'Hello'
@@ -40,6 +48,12 @@ class HelloWorldApi(remote.Service):
       path = "sayHelloByName", http_method='GET', name = "sayHelloByName")
     def say_hello_by_name(self, request):
       greet = "Hello {}".format(request.name)
+      return Hello(greeting=greet)
+
+    @endpoints.method(REQUEST_GREETING_CONTAINER, Hello,
+      path = "greetByPeriod", http_method='GET', name = "greetByPeriod")
+    def greet_by_period(self, request):
+      greet = "Good {} {}".format(request.period, request.name)
       return Hello(greeting=greet)
 
 
