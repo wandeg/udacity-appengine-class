d676c404f0cb2a8a4326e1b1d432de667faf30e0
wandeg <jmsmaringa@gmail.com>
2016-03-29 20:31:18 +0300

    Modify settings.py to read client id from confs.py

--- settings.py
+++ settings.py
@@ -10,7 +10,9 @@ created/forked from conference.py by wesc on 2014 may 24
 
 """
 
+from confs import CLIENT_ID
+
 # Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.
-WEB_CLIENT_ID = 'replace with Web client ID'
+WEB_CLIENT_ID = CLIENT_ID
 
